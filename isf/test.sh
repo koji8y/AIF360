467799a569a259258794558c2a68bf2ee2ba2dc7
Koji Yamamoto <kouji@acm.org>
2024-01-11 10:52:51 +0900

    debug: add information to test invoker script.
    Signed-off-by: Koji Yamamoto <kouji@acm.org>

--- isf/test.sh
+++ isf/test.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+set -x
 python -m isf.tests.unit_test -v TestStringMethods.test01_AdversarialDebiasing
+#python -m isf.tests.unit_test -v TestStringMethods.test02_EqualizedOdds
+#python -m isf.tests.unit_test -v TestStringMethods.test03_Massaging
+#python -m isf.tests.unit_test -v TestStringMethods.test04_RejectOptionClassification
+#python -m isf.tests.unit_test -v TestStringMethods.test05_Massaging_AA
